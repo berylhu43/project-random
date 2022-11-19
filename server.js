21780c948041217008ed285b9f04393a14e7e70c
berylhu43 <beryl.hu@yahoo.com>
2022-11-18 18:23:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const frontend_file = express.static(`${__dirname}/frontend_file`)
 app.use(frontend_file)
 
 app.listen(port, function() {
-	console.log(`reunning the server on port ${port}! visit http://localhost:${port}`)
+	console.log(`running the server on port ${port}! visit http://localhost:${port}`)
 })
 
